a4426d9f226eba6424b0cf807b1e44cc364b1312
Dilnaz <dilnaz.m13@gmail.com>
2025-05-09 23:50:11 +0500

    Fix staticfiles dirs for Render deployment

--- store/settings.py
+++ store/settings.py
@@ -114,6 +114,11 @@ STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+STATICFILES_DIRS = [
+    BASE_DIR / "stock" / "static",
+]
+
+
 # === Медиа ===
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
