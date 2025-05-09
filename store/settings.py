e54448d09e453e36ab91ea626076132e0a031e14
Dilnaz <dilnaz.m13@gmail.com>
2025-05-09 23:14:39 +0500

    Fix STATICFILES_DIRS for Render

--- store/settings.py
+++ store/settings.py
@@ -101,9 +101,10 @@ USE_I18N = True
 USE_L10N = True
 USE_TZ = True
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
+# STATICFILES_DIRS = [
+#    BASE_DIR / "static",
+# ]
+
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
