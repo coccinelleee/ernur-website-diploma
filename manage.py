088c305658bbc4030fdc3bbfad2defa51bf218b9
Dilnaz <dilnaz.m13@gmail.com>
2025-05-11 19:44:57 +0500

    Fix: enable user registration and login functionality

--- manage.py
+++ manage.py
@@ -1,39 +1,37 @@
 #!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
+"""Утилита командной строки Django для административных задач."""
 import os
 import sys
 
+def create_demo_superuser():
+    import django
+    django.setup()
+    from accounts.models import Account
+    if not Account.objects.filter(username='Admin06').exists():
+        Account.objects.create_superuser(
+            электрондық_пошта='admin06ernur@mail.ru',
+            username='Admin06',
+            аты_жөні='Admin06 Ernur',
+            тегі='Ernur067',
+            password='Ernur2025'
+        )
 
 def main():
-    """Run administrative tasks."""
+    """Выполнение административных задач."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'store.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
         raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
+            "Не удалось импортировать Django. Убедитесь, что он установлен и "
+            "доступен в переменной окружения PYTHONPATH. Вы забыли активировать виртуальное окружение?"
         ) from exc
-
-    # Отложенное создание суперпользователя после миграций
-    if len(sys.argv) > 1 and sys.argv[1] == 'migrate':
-        execute_from_command_line(sys.argv)
-        import django
-        django.setup()
-        from accounts.models import Account
-        if not Account.objects.filter(username='Admin06').exists():
-            Account.objects.create_superuser(
-                электрондық_пошта='admin06ernur@mail.ru',
-                username='Admin06',
-                аты_жөні='Admin06 Ernur',
-                тегі='Ernur067',
-                password='Ernur2025'
-            )
-        return
 
     execute_from_command_line(sys.argv)
 
+    # Создание суперпользователя после применения миграций
+    if len(sys.argv) > 1 and sys.argv[1] == 'migrate':
+        create_demo_superuser()
 
 if __name__ == '__main__':
     main()
