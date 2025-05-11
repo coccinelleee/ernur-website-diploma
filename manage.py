099b421d206feed2294eadb8f2c1e870b1f6cf81
Dilnaz <dilnaz.m13@gmail.com>
2025-05-11 16:59:52 +0500

    Auto-create demo superuser in manage.py

--- manage.py
+++ manage.py
@@ -2,6 +2,8 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+from accounts.models import Account
+from django.core.management import call_command
 
 
 def main():
@@ -18,5 +20,19 @@ def main():
     execute_from_command_line(sys.argv)
 
 
+def create_demo_superuser():
+    if not Account.objects.filter(username='Admin06').exists():
+        Account.objects.create_superuser(
+            электрондық_пошта='admin06ernur@mail.ru',
+            username='Admin06',
+            аты_жөні='Admin06 Ernur',
+            тегі='Ernur067',
+            password='Ernur2025'
+        )
+
 if __name__ == '__main__':
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'store.settings')
+    import django
+    django.setup()
+    create_demo_superuser()
     main()
